a7d25048fffeb47dff3e8285002ba46ca02078a7
deVlmer <bpier@zgamersa.com>
2026-08-25 01:39:32 -0400

    feat: block context menu and text selection

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+const preventContextMenu = (event: MouseEvent) => {
+  event.preventDefault();
+};
+
+document.addEventListener('contextmenu', preventContextMenu);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
